d14fea951fd15e7d8ffac1ab7f7c4a5e8573267c
zhubw <hyberwin@163.com>
2018-08-23 00:44:51 +0800

    测试java Double数据类型操作

---
+++ java-learning-demos/lan-spec-demos/src/test/java/NumberOperationTester.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class NumberOperationTester {
+    @Test
+    public void test(){
+        double a = .3 - .2;
+        double b = .2 - .1;
+        boolean test = (a==b);
+    }
+}
